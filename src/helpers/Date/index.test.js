cf7240851f4b49ea9c38361a1a10e605a3d75cf5
VincentC27 <cardon.vincent@hotmail.fr>
2024-07-19 14:57:19 +0200

    clean date helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -2,15 +2,13 @@
  * 
  */
 
-import { getMonth } from ".";
-
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            expect(getMonth(new Date("2022-01-01"))).toBe("janvier")
+            // to implement
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            expect(getMonth(new Date("2022-07-08"))).toBe("juillet")
+            // to implement
         });
     });
 })
